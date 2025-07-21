c11736281fd9a7ea0096def8d34244481051f9a8
kaleemullah <kaleemullahms78@gamil.com>
2025-07-21 15:37:04 +0500

    register user

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ import userRouter from "./routes/user.router.js"
 //routes declaration
 app.use("/api/v1/users", userRouter)
 
-// http://localhost:8000/api/v1/users/register
+
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 ConnectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`App is Listening on ${process.env.PORT}`);
+        console.log(`Server is Listening on PORT: ${process.env.PORT}`);
     })
 })
 .catch((err) => {
